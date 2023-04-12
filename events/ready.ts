3151c010b060516d5019128cd3c1539a5faa8c93
VakotUA <90602514+VakotUA@users.noreply.github.com>
2023-04-12 12:43:51 +0300

    Avatar
    + add new avatar
    
    ^ change onReady event handler

--- events/ready.ts
+++ events/ready.ts
@@ -5,16 +5,20 @@ import { logger } from '../utils/logger'
 export default <IEvent>{
   name: Events.ClientReady,
   async execute(client) {
-    logger.log('Status updating')
+    await (async () =>
+      client.user.setActivity({
+        name: 'Skyline',
+        type: ActivityType.Watching,
+        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      }))()
+      .then(() => logger.log('Status updated'))
+      .catch(logger.error)
 
-    await client.user.setActivity({
-      name: 'Skyline',
-      type: ActivityType.Watching,
-      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-    })
+    await client.user
+      .setAvatar('./assets/avatar.png')
+      .then(() => logger.log('Avatar updated'))
+      .catch(logger.error)
 
     logger.log(`Logged in as ${client.user!.tag}!`)
-
-    logger.log('Status updated')
   },
 }

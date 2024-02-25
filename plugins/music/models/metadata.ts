699a3e3ea1c0468b03e9b3720c9a6aafaf3ca6a4
VakotUA <vakotua.g@gmail.com>
2024-02-25 13:08:24 +0200

    scary one
    - remove almost ALL local actions and associated files
    ^ update packages to use completely with yarn

--- plugins/music/models/metadata.ts
+++
@@ -1,8 +0,0 @@
-import { GuildTextBasedChannel, Message, Snowflake } from 'discord.js'
-
-export interface IMetaData {
-  channel: GuildTextBasedChannel
-  message: Message | null
-  interval: NodeJS.Timer | null
-  skipVotes: Snowflake[]
-}

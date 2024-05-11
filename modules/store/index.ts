000513d7f77d864c91392b164295592fafa26124
VakotUA <vakotua.g@gmail.com>
2024-05-11 14:28:59 +0300

    fix: rtk api not in store

--- modules/store/index.ts
+++ modules/store/index.ts
@@ -1,29 +1,32 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
 
-import { categoryApi } from '@modules/api/action-category/action-category.api'
 import { actionApi } from '@modules/api/action/action.api'
+import { categoryApi } from '@modules/api/category/category.api'
 import { clientApi } from '@modules/api/client/client.api'
 import { commandApi } from '@modules/api/command/command.api'
 import { guildApi } from '@modules/api/guild/guild.api'
 import { listenerApi } from '@modules/api/listener/listener.api'
+import { permissionApi } from '@modules/api/permission/permission.api'
 
 export const store = configureStore({
   reducer: {
-    [clientApi.reducerPath]: clientApi.reducer,
-    [guildApi.reducerPath]: guildApi.reducer,
     [actionApi.reducerPath]: actionApi.reducer,
     [categoryApi.reducerPath]: categoryApi.reducer,
+    [clientApi.reducerPath]: clientApi.reducer,
     [commandApi.reducerPath]: commandApi.reducer,
+    [guildApi.reducerPath]: guildApi.reducer,
     [listenerApi.reducerPath]: listenerApi.reducer,
+    [permissionApi.reducerPath]: permissionApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
-      .concat(clientApi.middleware)
-      .concat(guildApi.middleware)
       .concat(actionApi.middleware)
       .concat(categoryApi.middleware)
+      .concat(clientApi.middleware)
       .concat(commandApi.middleware)
-      .concat(listenerApi.middleware),
+      .concat(guildApi.middleware)
+      .concat(listenerApi.middleware)
+      .concat(permissionApi.middleware),
 })
 
 export type AppDispatch = typeof store.dispatch
